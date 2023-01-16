8facd7408fe91d07e085d65fba48f09bcc516a57
Ilona Shishov <Ilona.Shishov@gmail.com>
2023-01-16 07:22:06 +0200

    chore: defined simple deployer
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>

--- replica-builder/deployer/pkg/deployer/deployer.go
+++ replica-builder/deployer/pkg/deployer/deployer.go
@@ -1,11 +1,27 @@
 package deployer
 
 import (
-	"fmt"
+	"log"
+	"os/exec"
 
 	"github.com/RHEcosystemAppEng/SaaSi/replica-builder/deployer/pkg/packager"
+	"github.com/RHEcosystemAppEng/SaaSi/replica-builder/deployer/pkg/utils"
+)
+
+var (
+	err error
 )
 
 func NewDeployment(pkg *packager.ApplicationPkg) {
-	fmt.Println("deploying application....")
+	
+	// validate kustomize cli
+	utils.ValidateRequirements()
+
+	// set the namespace resource to target namespace
+	cmd := exec.Command("oc", "apply", "-f", ".")
+	cmd.Dir = pkg.DeloymentDir
+	if err = cmd.Run(); err != nil {
+		log.Fatalf("Failed deploy files from deployment directory: %s", pkg.DeloymentDir)
+	}
+	log.Printf("Successfully deployed all files from deployment directory: %s", pkg.DeloymentDir)
 }
