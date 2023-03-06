3f6ac9472149534600ad431cc03b19af5e254db6
Zvi Grinberg <zgrinber@redhat.com>
2023-03-06 09:04:26 +0200

    revert to former way to supply directory input to oc apply command
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- deployer/pkg/deployer/app/deployer/deploy.go
+++ deployer/pkg/deployer/app/deployer/deploy.go
@@ -2,7 +2,6 @@ package deployer
 
 import (
 	"log"
-	"os"
 	"os/exec"
 
 	"github.com/RHEcosystemAppEng/SaaSi/deployer/pkg/deployer/app/packager"
@@ -19,8 +18,8 @@ func DeployApplication(pkg *packager.ApplicationPkg) {
 	utils.ValidateRequirements(utils.OC)
 
 	// deploy application target namespaces using oc cli
-	cmd := exec.Command("oc", "apply", "-f", pkg.TargetNamespaceDir, "--kubeconfig", pkg.DeployerContext.GetKubeConfigPath())
-	cmd.Dir, _ = os.Getwd()
+	cmd := exec.Command("oc", "apply", "-f", ".", "--kubeconfig", pkg.DeployerContext.GetKubeConfigPath())
+	cmd.Dir = pkg.TargetNamespaceDir
 	if err = cmd.Run(); err != nil {
 		log.Fatalf("Failed to deploy files from target namespace directory: %s, Error: %s", pkg.TargetNamespaceDir, err)
 	}
